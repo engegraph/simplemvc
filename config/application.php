53e34f1aa752af452713c6fa571a0001967a6dfe
Airton <Airton@engegraph.com.br>
2017-12-29 16:58:46 -0200

    Configurações Iniciais

---
+++ config/application.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: airton.lopes
+ * Date: 29/12/2017
+ * Time: 16:57
+ */
---
+++ core/Router.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: airton.lopes
+ * Date: 29/12/2017
+ * Time: 16:56
+ */
+
+namespace Core;
+
+
+class Router
+{
+
+}
